3ec707fc941005ca1aaeeb0128e5c0c16c0e056d
ONYEDIKACHUKWU CHIEDU <77061016+chieduchineme@users.noreply.github.com>
2025-05-13 16:26:04 +0200

    Update order_main.py

--- AI_Backend/app/order/order_main.py
+++ AI_Backend/app/order/order_main.py
@@ -23,4 +23,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("order_main:app", host="127.0.0.1", port=8000, reload=True)
